ab2b71883d801ca7738637b01e27101f61a4f506
moura1001 <gmouraneto01@gmail.com>
2024-08-25 13:42:21 -0300

    fix: erro de sintaxe nos blocos

--- terraform/5-nat.tf
+++ terraform/5-nat.tf
@@ -8,7 +8,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id = aws.subnet.public_zone1.id
+  subnet_id = aws_subnet.public_zone1.id
 
   tags = {
     Name = "${local.env}-nat"
--- terraform/6-routes.tf
+++ terraform/6-routes.tf
@@ -1,7 +1,7 @@
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route = {
+  route {
     cidr_block      = "0.0.0.0/0"
     nat_gateway_id  = aws_nat_gateway.nat.id
   }
@@ -14,7 +14,7 @@ resource "aws_route_table" "private" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route = {
+  route {
     cidr_block  = "0.0.0.0/0"
     gateway_id  = aws_internet_gateway.igw.id
   }
